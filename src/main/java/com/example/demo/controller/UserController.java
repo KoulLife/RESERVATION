1ff2a1575561e3a9fe527ad82c470bfcabb66322
Koul <dongik0122@gmail.com>
2024-04-29 18:20:26 +0900

    v1
    v1

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDto;
+import com.example.demo.model.RoleType;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+  @Autowired
+  UserService userService;
+
+  @PostMapping("/register")
+  public void register(UserDto userDto){
+    userService.submit(userDto);
+  }
+
+  @PostMapping("/access-expert")
+  public void accessExpert(Long id){
+    userService.changeToExpert(id);
+  }
+
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.UserRepository;
+import com.example.demo.dto.UserDto;
+import com.example.demo.model.RoleType;
+import com.example.demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+  @Autowired
+  UserRepository userRepository;
+
+  public void submit(UserDto userDto){
+    String username = userDto.getUsername();
+    String password = userDto.getPassword();
+    RoleType role = RoleType.USER;
+
+    User user = new User();
+    user.setUsername(username);
+    user.setPassword(password);
+    user.setRoleType(role);
+
+    userRepository.save(user);
+  }
+
+  @Transactional
+  public void changeToExpert(Long id){
+    Optional<User> user = userRepository.findById(id);
+    user.get().setRoleType(RoleType.EXPERT);
+  }
+}
